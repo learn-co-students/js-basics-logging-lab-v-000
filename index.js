6a85b0452f5aad3428f7f5bcd93dd4ca877baf65
epic0448 <samuelogundiran@Yahoo.com>
2018-11-25 22:27:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const driver = 'bob';
+
+console.log('this code was called')
+console.log (driver) 
+// driver was already decleared.. so we dont need to introplate it. 
+console.log (driver , `is the driver variable name`)
