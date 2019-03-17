85633b00a246d500657930162233470b3cf0d5b8
James Borocz <thesilentc@me.com>
2019-03-17 13:09:42 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const driver = 'bob';
 console.log("this code was called");
 console.log(driver);
 console.log('bob', 'is the driver variable name');
+// console.log(driver, 'is the driver variable name');
